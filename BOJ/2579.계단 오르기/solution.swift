e0819cdf000ba3b4e120377336658e4328dd2ec4
Jaehoon Jang <jangjh971008@naver.com>
2022-04-17 15:47:56 +0900

    update boj 2579 swift

--- BOJ/2579.계단 오르기/solution.swift
+++ BOJ/2579.계단 오르기/solution.swift
@@ -2,24 +2,26 @@ import Foundation
 
 let n: Int = Int(readLine()!)!
 
-var stairs: [Int] = Array(repeating: 0, count: n + 1)
+var stairs: [Int] = [0]
+var DP: [Int] = Array(repeating: 0, count: n+1)
 
-var DP: [Int] = Array(repeating: 0, count: n + 1)
+for _ in 0 ..< n {
+    stairs.append(Int(readLine()!)!)
+}
 
-var count: Int = 3
-
-for i in 1 ... n {
-    stairs[i] += Int(readLine()!)!
+if n == 1 {
+    print(stairs[1])
+    exit(0)
+} else if n == 2 {
+    print(stairs[1] + stairs[2])
+    exit(0)
 }
 
 DP[1] = stairs[1]
-DP[2] = DP[1] + stairs[2]
+DP[2] = stairs[1] + stairs[2]
 
 for i in 3 ... n {
-    DP[i] = max(
-        DP[i - 2],
-        DP[i - 3] + stairs[i - 1]
-    ) + stairs[i]
+    DP[i] = max(DP[i - 2], stairs[i - 1] + DP[i - 3]) + stairs[i]
 }
 
-print(DP.removeLast())
+print(DP.last!)
--- Workspace/Algorithm/Algorithm/main.swift
+++ Workspace/Algorithm/Algorithm/main.swift
@@ -1,93 +1,27 @@
 import Foundation
 
-// 입력 시작
 let n: Int = Int(readLine()!)!
 
-var board = Array(repeating: Array(repeating: 0, count: n), count: n)
+var stairs: [Int] = [0]
+var DP: [Int] = Array(repeating: 0, count: n+1)
 
-let appleCount: Int = Int(readLine()!)!
-
-for _ in 0 ..< appleCount {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-
-    board[input[0] - 1][input[1] - 1] = 1
+for _ in 0 ..< n {
+    stairs.append(Int(readLine()!)!)
 }
 
-let directionCount: Int = Int(readLine()!)!
-var directions: [Direction] = []
-
-for _ in 0 ..< directionCount {
-    let input = readLine()!.split(separator: " ").map { String($0) }
-
-    directions.append(Direction(block: Int(input[0])!, direction: input[1]))
+if n == 1 {
+    print(stairs[1])
+    exit(0)
+} else if n == 2 {
+    print(stairs[1] + stairs[2])
+    exit(0)
 }
 
-// 입력 끝
+DP[1] = stairs[1]
+DP[2] = stairs[1] + stairs[2]
 
-var snake: [Point] = [Point(i: 0, j: 0)]
-var (si, sj, d) = (0, 0, 0)
-
-var delta = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-
-var change: Direction? = directions.removeFirst()
-var count = 0
-
-solution()
-
-// MARK: - 선언
-
-struct Direction {
-    var block: Int
-    var direction: String
+for i in 3 ... n {
+    DP[i] = max(DP[i - 2], stairs[i - 1] + DP[i - 3]) + stairs[i]
 }
 
-struct Point {
-    var i: Int
-    var j: Int
-}
-
-func solution() {
-    while isInBound() {
-        if change != nil && change!.block == count {
-            if change!.direction == "D" {
-                d += 1
-            } else {
-                d -= 1
-                if d < 0 {
-                    d += 4
-                }
-            }
-
-            if directions.isEmpty {
-                change = nil
-            } else {
-                change = directions.removeFirst()
-            }
-        }
-
-        si += delta[d % 4][0]
-        sj += delta[d % 4][1]
-
-        if snake.contains(where: { $0.i == si && $0.j == sj }) {
-            break
-        } else if !isInBound() {
-            break
-        }
-
-        if board[si][sj] == 0 {
-            snake.insert(Point(i: si, j: sj), at: 0)
-            snake.removeLast()
-        } else {
-            snake.insert(Point(i: si, j: sj), at: 0)
-            board[si][sj] = 0
-        }
-
-        count += 1
-    }
-
-    print(count + 1)
-}
-
-func isInBound() -> Bool {
-    return 0 <= si && si < n && 0 <= sj && sj < n
-}
+print(DP.last!)
