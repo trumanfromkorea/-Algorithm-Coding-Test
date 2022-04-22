9eb63b07870363e2565fa97536d3a374c4aae0a6
Jaehoon Jang <jangjh971008@naver.com>
2022-04-23 03:28:27 +0900

    욕심쟁이 판다 푸는중

--- Workspace/Algorithm/Algorithm/heap.swift
+++ Workspace/Algorithm/Algorithm/heap.swift
@@ -6,175 +6,177 @@
 //
 
 import Foundation
-
-struct Heap<T: Comparable> {
-    var heap: [T]
-
-    // maxHeap >, minHeap <
-    var compare: (T, T) -> Bool
-
-    var root: T? {
-        return heap.isEmpty ? nil : heap[0]
-    }
-    
-    var isEmpty: Bool {
-        return heap.isEmpty
-    }
-
-    init(compare: @escaping (T, T) -> Bool) {
-        self.heap = []
-        self.compare = compare
-    }
-
-    // 삽입
-    mutating func insert(n: T) {
-        heap.append(n)
-        shiftUp(i: heap.count - 1)
-    }
-    
-    // 삭제
-    mutating func delete() -> T? {
-        if heap.isEmpty {
-            return nil
-        }
-        
-        if heap.count == 1 {
-            return heap.removeFirst()
-        }
-        
-        heap.swapAt(0, heap.count - 1)
-        
-        let result = heap.removeLast()
-        shiftDown(i: 0)
-        
-        return result
-    }
-    
-    // 삽입 시 노드 상승
-    mutating func shiftUp(i: Int) {
-        var now = i
-
-        // 현재 노드가 루트 노드보다 하위 노드일때
-        while now > 0 {
-            // 부모 노드 인덱스
-            let parent = (now - 1) / 2
-
-            // 부모 노드와 대소비교, 교환 필요 시
-            if compare(heap[now], heap[parent]) {
-                heap.swapAt(now, parent)
-                // 교환 후 현재 노드를 원래 부모 노드가 있던 자리로 옮겨줌
-                now = parent
-            } else {
-                break
-            }
-        }
-    }
-
-    // 루트노드 값 제자리 찾기 (원래 원소 삭제 시 많이 쓰임)
-    mutating func shiftDown(i: Int) {
-        var now: Int = i
-        // 자식 노드 인덱스
-        var child: Int = 2 * now + 1
-        let count: Int = heap.count
-
-        // 자식 노드가 트리 범위안에 있을때
-        while child < count {
-            // 왼쪽 자식, 오른쪽 자식 둘다 있을때
-            if child + 1 < count {
-                child = compare(heap[child], heap[child + 1]) ? child : child + 1
-            }
-            // 자식 노드와 대소비교 후 교환
-            if compare(heap[child], heap[now]) {
-                heap.swapAt(now, child)
-                now = child // 현재 노드를 자식노드 인덱스로
-                child = 2 * now + 1
-            } else {
-                break
-            }
-        }
-    }
-}
-
+//
 //struct Heap<T: Comparable> {
-//    var nodes: [T] = []
-//    let sort: (T,T) -> Bool
+//    var heap: [T]
 //
-//    init(sort: @escaping (T,T) -> Bool) {
-//        self.sort = sort
+//    // maxHeap >, minHeap <
+//    var compare: (T, T) -> Bool
+//
+//    var root: T? {
+//        return heap.isEmpty ? nil : heap[0]
 //    }
 //
-//    // Heap에 데이터 추가 (push)
-//    mutating func insert(_ element: T) {
-//        let count = nodes.count
-//        nodes.append(element)
-//
-//        up(count - 1)
+//    var isEmpty: Bool {
+//        return heap.isEmpty
 //    }
 //
-//    // Heap에서 데이터 꺼내면서 삭제 (pop)
+//    init(compare: @escaping (T, T) -> Bool) {
+//        self.heap = []
+//        self.compare = compare
+//    }
+//
+//    // 삽입
+//    mutating func insert(n: T) {
+//        heap.append(n)
+//        shiftUp(i: heap.count - 1)
+//    }
+//
+//    // 삭제
 //    mutating func delete() -> T? {
-//        if nodes.isEmpty {
+//        if heap.isEmpty {
 //            return nil
 //        }
-//        if nodes.count == 1 {
-//            return nodes.removeFirst()
-//        }
-//        nodes.swapAt(0, nodes.count - 1)
 //
-//        let result = nodes.removeLast()
-//        down(0)
+//        if heap.count == 1 {
+//            return heap.removeFirst()
+//        }
+//
+//        heap.swapAt(0, heap.count - 1)
+//
+//        let result = heap.removeLast()
+//        shiftDown(i: 0)
 //
 //        return result
 //    }
 //
-//    // Heap에서 특정 데이터 삭제
-//    mutating func remove(_ element: T) {
-//        if let index = nodes.firstIndex(of: element) {
-//            nodes.swapAt(index, nodes.count - 1)
-//            nodes.removeLast()
-//            up(index)
-//            down(index)
-//        }
-//    }
+//    // 삽입 시 노드 상승
+//    mutating func shiftUp(i: Int) {
+//        var now = i
 //
-//    // Heap에서 데이터 전체 삭제
-//    mutating func removeAll(_ element: T) {
-//        var count = nodes.count
-//        remove(element)
-//        while nodes.count < count {
-//            remove(element)
-//            count = nodes.count
-//        }
-//    }
+//        // 현재 노드가 루트 노드보다 하위 노드일때
+//        while now > 0 {
+//            // 부모 노드 인덱스
+//            let parent = (now - 1) / 2
 //
-//    // Heap에서 첫 데이터 pop
-//    func peek() -> T? {
-//        return nodes.first
-//    }
-//
-//    // Heap 데이터 아래방향으로 비교 정렬
-//    mutating func down(_ index: Int) {
-//        var index = index
-//        let count = nodes.count
-//        while 2 * index + 1 < count {
-//            var i = 2 * index + 1
-//            if i < (count - 1) && sort(nodes[i], nodes[i + 1]) {
-//                i += 1
-//            }
-//            if !sort(nodes[index], nodes[i]) {
+//            // 부모 노드와 대소비교, 교환 필요 시
+//            if compare(heap[now], heap[parent]) {
+//                heap.swapAt(now, parent)
+//                // 교환 후 현재 노드를 원래 부모 노드가 있던 자리로 옮겨줌
+//                now = parent
+//            } else {
 //                break
 //            }
-//            nodes.swapAt(index, i)
-//            index = i
 //        }
 //    }
 //
-//    // Heap 데이터 윗방향으로 비교 정렬
-//    mutating func up(_ index: Int) {
-//        var index = index
-//        while index > 0 && !sort(nodes[(index - 1) / 2], nodes[index]) {
-//            nodes.swapAt((index - 1) / 2, index)
-//            index = (index - 1) / 2
+//    // 루트노드 값 제자리 찾기 (원래 원소 삭제 시 많이 쓰임)
+//    mutating func shiftDown(i: Int) {
+//        var now: Int = i
+//        // 자식 노드 인덱스
+//        var child: Int = 2 * now + 1
+//        let count: Int = heap.count
+//
+//        // 자식 노드가 트리 범위안에 있을때
+//        while child < count {
+//            // 왼쪽 자식, 오른쪽 자식 둘다 있을때
+//            if child + 1 < count {
+//                child = compare(heap[child], heap[child + 1]) ? child : child + 1
+//            }
+//            // 자식 노드와 대소비교 후 교환
+//            if compare(heap[child], heap[now]) {
+//                heap.swapAt(now, child)
+//                now = child // 현재 노드를 자식노드 인덱스로
+//                child = 2 * now + 1
+//            } else {
+//                break
+//            }
 //        }
 //    }
 //}
+
+var heap: Heap = Heap<Int>(sort: >)
+
+struct Heap<T: Comparable> {
+    var nodes: [T] = []
+    let sort: (T,T) -> Bool
+
+    init(sort: @escaping (T,T) -> Bool) {
+        self.sort = sort
+    }
+
+    // Heap에 데이터 추가 (push)
+    mutating func insert(_ element: T) {
+        let count = nodes.count
+        nodes.append(element)
+
+        up(count - 1)
+    }
+
+    // Heap에서 데이터 꺼내면서 삭제 (pop)
+    mutating func delete() -> T? {
+        if nodes.isEmpty {
+            return nil
+        }
+        if nodes.count == 1 {
+            return nodes.removeFirst()
+        }
+        nodes.swapAt(0, nodes.count - 1)
+
+        let result = nodes.removeLast()
+        down(0)
+
+        return result
+    }
+
+    // Heap에서 특정 데이터 삭제
+    mutating func remove(_ element: T) {
+        if let index = nodes.firstIndex(of: element) {
+            nodes.swapAt(index, nodes.count - 1)
+            nodes.removeLast()
+            up(index)
+            down(index)
+        }
+    }
+
+    // Heap에서 데이터 전체 삭제
+    mutating func removeAll(_ element: T) {
+        var count = nodes.count
+        remove(element)
+        while nodes.count < count {
+            remove(element)
+            count = nodes.count
+        }
+    }
+
+    // Heap에서 첫 데이터 pop
+    func peek() -> T? {
+        return nodes.first
+    }
+
+    // Heap 데이터 아래방향으로 비교 정렬
+    mutating func down(_ index: Int) {
+        var index = index
+        let count = nodes.count
+        while 2 * index + 1 < count {
+            var i = 2 * index + 1
+            if i < (count - 1) && sort(nodes[i], nodes[i + 1]) {
+                i += 1
+            }
+            if !sort(nodes[index], nodes[i]) {
+                break
+            }
+            nodes.swapAt(index, i)
+            index = i
+        }
+    }
+
+    // Heap 데이터 윗방향으로 비교 정렬
+    mutating func up(_ index: Int) {
+        var index = index
+        while index > 0 && !sort(nodes[(index - 1) / 2], nodes[index]) {
+            nodes.swapAt((index - 1) / 2, index)
+            index = (index - 1) / 2
+        }
+    }
+}
--- Workspace/Algorithm/Algorithm/main.swift
+++ Workspace/Algorithm/Algorithm/main.swift
@@ -1,1 +1,72 @@
 import Foundation
+
+let n: Int = Int(readLine()!)!
+
+var answer: Int = 0
+var forest: [[Int]] = []
+var DP: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: n)
+
+let di = [0, 0, -1, 1]
+let dj = [-1, 1, 0, 0]
+
+for _ in 0 ..< n {
+    let line = readLine()!.split(separator: " ").map { Int($0)! }
+    forest.append(line)
+}
+
+for i in 0 ..< n {
+    for j in 0 ..< n {
+        BFS(i, j)
+    }
+}
+
+for line in DP {
+    answer = max(answer, line.max()!)
+}
+
+print(answer + 1)
+
+// MARK: - 선언
+
+struct Point {
+    var i: Int
+    var j: Int
+    var distance: Int
+}
+
+func BFS(_ i: Int, _ j: Int) {
+    var queue: DoubleStackQueue = DoubleStackQueue<Point>()
+
+    queue.enqueue(Point(i: i, j: j, distance: 0))
+
+    while !queue.isEmpty {
+        let now = queue.dequeue()!
+        
+        DP[i][j] = max(now.distance, DP[i][j])
+        
+        for line in 0 ... i {
+            answer = max(answer, DP[line].max()!)
+        }
+        
+        for k in 0 ..< 4 {
+            let ni = now.i + di[k]
+            let nj = now.j + dj[k]
+
+            if !isInBound(ni, nj) {
+                continue
+            } else if forest[ni][nj] <= forest[now.i][now.j] {
+                continue
+            } else if DP[ni][nj] >= answer {
+                DP[i][j] = max(DP[ni][nj] + now.distance + 1, DP[i][j])
+                continue
+            }
+
+            queue.enqueue(Point(i: ni, j: nj, distance: now.distance + 1))
+
+        }
+    }
+}
+
+func isInBound(_ i: Int, _ j: Int) -> Bool {
+    return 0 <= i && i < n && 0 <= j && j < n
+}
