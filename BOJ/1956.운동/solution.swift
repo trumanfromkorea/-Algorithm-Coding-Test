88bfe08fcc464507aea7d971914eecae6654a4ad
Jaehoon Jang <jangjh971008@naver.com>
2023-01-04 13:12:44 +0900

    Update solution.swift

--- BOJ/1956.운동/solution.swift
+++ BOJ/1956.운동/solution.swift
@@ -1,4 +1,3 @@
-
 import Foundation
 
 let INF = 400 * 100000
@@ -14,12 +13,10 @@ for _ in 0 ..< e {
     graph[i][j] = w
 }
 
-var dist = graph
-
 for k in 0 ..< v {
     for i in 0 ..< v {
         for j in 0 ..< v {
-            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
         }
     }
 }
@@ -27,7 +24,7 @@ for k in 0 ..< v {
 var answer = INF
 
 for i in 0 ..< v {
-    answer = min(answer, dist[i][i])
+    answer = min(answer, graph[i][i])
 }
 
 print(answer == INF ? -1 : answer)
